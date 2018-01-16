d26df9f4cc383c5820c151366bf8eb324b112373
reloxx13 <reloxx@interia.pl>
2018-01-16 11:39:19 +0100

    fix create config

--- includes/Config.php
+++ includes/Config.php
@@ -1,64 +1,65 @@
 <?php
-
-class Config {
-	private $cfgFile = "data/MyConfig.php";
 	
-	private $defaultConfigs
-		= [
-			"ota_server_ip" => "",
-			"username"      => "",
-			"password"      => "",
-			"refreshtime"   => "2",
-		];
-	
-	function __construct() {
-		if( !file_exists( $this->cfgFile ) ) {
-			fopen( $this->cfgFile, 'w' ) or die(
-			__(
-				"ERROR_CANNOT_CREATE_FILE", "USER_CONFIG", [ "cfgFilePath" => $this->cfgFile ]
-			)
-			);
-			$config                    = $this->defaultConfigs;
-			$config[ "ota_server_ip" ] = __( "DEFAULT_HOST_IP_PLACEHOLDER", "USER_CONFIG" );
-			$config                    = var_export( $config, TRUE );
+	class Config {
+		private $cfgFile = _APPROOT_ . "data/MyConfig.php";
+		
+		private $defaultConfigs
+			= [
+				"ota_server_ip" => "",
+				"username"      => "",
+				"password"      => "",
+				"refreshtime"   => "2",
+			];
+		
+		function __construct() {
+			if( !file_exists( $this->cfgFile ) ) {
+				fopen( $this->cfgFile, 'w' ) or die(
+				__(
+					"ERROR_CANNOT_CREATE_FILE", "USER_CONFIG", [ "cfgFilePath" => $this->cfgFile ]
+				)
+				);
+				$config = $this->defaultConfigs;
+				
+				$config[ "ota_server_ip" ] = __( "DEFAULT_HOST_IP_PLACEHOLDER", "USER_CONFIG" );
+				$config                    = var_export( $config, TRUE );
+				file_put_contents( $this->cfgFile, "<?php return $config ; ?>" );
+			}
+			
+			foreach( $this->defaultConfigs as $configName => $configValue ) {
+				$config = $this->read( $configName );
+				if( !isset( $config ) ) {
+					$this->write( $configName, $configValue );
+				}
+			}
+			
+		}
+		
+		public function readAll() {
+			$config = include $this->cfgFile;
+			if( $config === 1 ) { //its empty
+				return [];
+			}
+			
+			return $config;
+		}
+		
+		public function read( $key ) {
+			$config = include $this->cfgFile;
+			if( $config === 1 ) { //its empty
+				return [];
+			}
+			
+			return isset( $config[ $key ] ) ? $config[ $key ] : null;
+		}
+		
+		public function write( $key, $value ) {
+			$config = include $this->cfgFile;
+			if( $config === 1 ) { //its empty
+				$config = [];
+			}
+			
+			$config[ $key ] = $value;
+			$config         = var_export( $config, TRUE );
 			file_put_contents( $this->cfgFile, "<?php return $config ; ?>" );
 		}
-		
-		foreach( $this->defaultConfigs as $configName => $configValue ) {
-			$config = $this->read( $configName );
-			if( !isset( $config ) ) {
-				$this->write( $configName, $configValue );
-			}
-		}
-		
 	}
-	
-	public function readAll() {
-		$config = include $this->cfgFile;
-		if( $config === 1 ) { //its empty
-			return [];
-		}
-		
-		return $config;
-	}
-	
-	public function read( $key ) {
-		$config = include $this->cfgFile;
-		if( $config === 1 ) { //its empty
-			return [];
-		}
-		
-		return isset( $config[ $key ] ) ? $config[ $key ] : null;
-	}
-	
-	public function write( $key, $value ) {
-		$config = include $this->cfgFile;
-		if( $config === 1 ) { //its empty
-			$config = [];
-		}
-		
-		$config[ $key ] = $value;
-		$config         = var_export( $config, TRUE );
-		file_put_contents( $this->cfgFile, "<?php return $config ; ?>" );
-	}
-}
--- includes/top.php
+++ includes/top.php
@@ -1,72 +1,75 @@
 <?php
-	ini_set( 'session.gc_maxlifetime', 2678400 );
-	session_start();
+ini_set( 'session.gc_maxlifetime', 2678400 );
+session_start();
+
+
+define( "_VERSION_", "1.0.1a" );
+
+define( "_APPROOT_", "./" );
+define( "_RESOURCESDIR_", _APPROOT_ . "resources/" );
+define( "_INCLUDESDIR_", _APPROOT_ . "includes/" );
+define( "_LIBSDIR_", _APPROOT_ . "libs/" );
+define( "_PAGESDIR_", _APPROOT_ . "pages/" );
+define( "_DATADIR_", _APPROOT_ . "data/" );
+define( "_LANGDIR_", _APPROOT_ . "lang/" );
+define( "_TMPDIR_", _APPROOT_ . "tmp/" );
+
+/**
+ * @property Sonoff Sonoff
+ */
+require_once _INCLUDESDIR_ . "Config.php";
+require_once _INCLUDESDIR_ . "Sonoff.php";
+require_once _LIBSDIR_ . 'phpi18n/i18n.class.php';
+require_once _INCLUDESDIR_ . "Config.php";
+
+$i18n = new i18n();
+
+$lang = isset( $_GET[ "lang" ] ) ? $_GET[ "lang" ] : null;
+if( isset( $lang ) ) {
+	$_SESSION[ 'lang' ] = $lang;
+}
+
+
+$i18n->setCachePath( _TMPDIR_ . 'cache/i18n/' );
+$i18n->setFilePath( _LANGDIR_ . 'lang_{LANGUAGE}.ini' ); // language file path
+$i18n->setFallbackLang( 'en' );
+$i18n->setPrefix( '__L' );
+$i18n->setSectionSeperator( '_' );
+$i18n->setMergeFallback( TRUE ); // make keys available from the fallback language
+$i18n->init();
+
+$lang = $i18n->getAppliedLang();
+
+
+$Config = new Config();
+
+
+function __( $string, $category = null, $args = null ) {
+	$cat = "";
+	if( isset( $category ) && !empty( $category ) ) {
+		$cat = $category . "_";
+	}
+	$txt        = $cat . $string;
+	$translated = @__L::$txt( $args );
 	
-	
-	define( "_VERSION_", "1.0.1a" );
-	
-	define( "_APPROOT_", "./" );
-	define( "_RESOURCESDIR_", _APPROOT_."resources/" );
-	define( "_INCLUDESDIR_", _APPROOT_."includes/" );
-	define( "_LIBSDIR_", _APPROOT_."libs/" );
-	define( "_PAGESDIR_", _APPROOT_."pages/" );
-	define( "_DATADIR_", _APPROOT_."data/" );
-	define( "_LANGDIR_", _APPROOT_."lang/" );
-	define( "_TMPDIR_", _APPROOT_."tmp/" );
-	
-	/**
-	 * @property Sonoff Sonoff
-	 */
-	require_once _INCLUDESDIR_."Config.php";
-	require_once _INCLUDESDIR_."Sonoff.php";
-	require_once _LIBSDIR_.'phpi18n/i18n.class.php';
-	require_once _INCLUDESDIR_."Config.php";
-	
-	$Config = new Config();
-	$i18n   = new i18n();
-	
-	$lang = isset( $_GET[ "lang" ] ) ? $_GET[ "lang" ] : NULL;
-	if ( isset( $lang ) ) {
-		$_SESSION[ 'lang' ] = $lang;
+	if( $translated == "" ) {
+		$myfile = fopen( _LANGDIR_ . "lang_new.ini", "a" ) or die( "Unable to open file!" );
+		$txt = "";
+		if( $category != "" ) {
+			$txt .= "\n[" . $category . "]\n";
+		}
+		$txt .= $string . " = \"MISSING_TRANSLATION\"\n";
+		fwrite( $myfile, $txt );
+		fclose( $myfile );
+		$translated = "MISSING_TRANSLATION";
+		$files      = glob( _TMPDIR_ . 'cache/i18n/*' ); // get all file names
+		foreach( $files as $file ) { // iterate files
+			if( is_file( $file ) ) {
+				unlink( $file );
+			}
+		}
+		
 	}
 	
-	
-	$i18n->setCachePath( _TMPDIR_.'cache/i18n/' );
-	$i18n->setFilePath( _LANGDIR_.'lang_{LANGUAGE}.ini' ); // language file path
-	$i18n->setFallbackLang( 'en' );
-	$i18n->setPrefix( '__L' );
-	$i18n->setSectionSeperator( '_' );
-	$i18n->setMergeFallback( TRUE ); // make keys available from the fallback language
-	$i18n->init();
-	
-	$lang = $i18n->getAppliedLang();
-	
-	function __( $string, $category = NULL, $args = NULL ) {
-		$cat = "";
-		if ( isset( $category ) && !empty( $category ) ) {
-			$cat = $category."_";
-		}
-		$txt        = $cat.$string;
-		$translated = @__L::$txt( $args );
-		
-		if ( $translated == "" ) {
-			$myfile = fopen( _LANGDIR_."lang_new.ini", "a" ) or die( "Unable to open file!" );
-			$txt = "";
-			if ( $category != "" ) {
-				$txt .= "\n[".$category."]\n";
-			}
-			$txt .= $string." = \"MISSING_TRANSLATION\"\n";
-			fwrite( $myfile, $txt );
-			fclose( $myfile );
-			$translated = "MISSING_TRANSLATION";
-			$files      = glob( _TMPDIR_.'cache/i18n/*' ); // get all file names
-			foreach ( $files as $file ) { // iterate files
-				if ( is_file( $file ) ) {
-					unlink( $file );
-				}
-			}
-			
-		}
-		
-		return $translated;
-	}
+	return $translated;
+}
