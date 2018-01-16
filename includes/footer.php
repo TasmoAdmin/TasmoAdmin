240f3fd2665bf1044731dd33caaa334670ce97e6
reloxx13 <reloxx@interia.pl>
2018-01-16 21:55:09 +0100

    add github link

--- includes/footer.php
+++ includes/footer.php
@@ -1,6 +1,9 @@
 <div class='clear'></div>
 <div id='footer'>
-    &copy; <?php echo date( "Y" ); ?> <?php echo __( "BY" ); ?> reloxx13 - V<?php echo _VERSION_; ?>
+	&copy; <?php echo date( "Y" ); ?> <?php echo __( "BY" ); ?> reloxx13 - V<?php echo _VERSION_; ?> -
+	<a href='https://github.com/reloxx13/SonWEB' title='SonWEB <?php echo __( "VIEW_ON_GITHUB" ); ?>' target='_blank'>
+		<?php echo __( "VIEW_ON_GITHUB" ); ?>
+	</a>
 </div>
 </body>
 </html>
