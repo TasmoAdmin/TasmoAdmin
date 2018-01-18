0e87855db8882cf331d0b6168382d062994cd500
reloxx13 <reloxx@interia.pl>
2018-01-18 21:44:19 +0100

    fix config slow reading

--- includes/Config.php
+++ includes/Config.php
@@ -14,7 +14,6 @@
 		
 		function __construct() {
 			if ( !file_exists( $this->cfgFile ) ) { //create file if not exists
-				
 				$fh = fopen( $this->cfgFile, 'w+' ) or die(
 				__(
 					"ERROR_CANNOT_CREATE_FILE",
@@ -27,7 +26,7 @@
 				$config[ "ota_server_ip" ] = __( "DEFAULT_HOST_IP_PLACEHOLDER", "USER_CONFIG" );
 				$config                    = var_export( $config, TRUE );
 				if ( !fwrite( $fh, "<?php return $config ; ?>" ) ) {
-					die( "COULD NOT CREATE ORWRITE IN CONFIG FILE" );
+					die( "COULD NOT CREATE OR WRITE IN CONFIG FILE" );
 				}
 				fclose( $fh );
 			}
@@ -52,8 +51,8 @@
 		}
 		
 		public function read( $key ) {
-			$config = include $this->cfgFile;
-			
+			$config = file_get_contents( $this->cfgFile );
+			$config = eval( "?>".$config."<?php" );
 			if ( $config === 1 ) { //its empty
 				$config = [];
 			}
@@ -70,17 +69,7 @@
 			
 			$config[ $key ] = $value;
 			$config         = var_export( $config, TRUE );
-			$fh = fopen( $this->cfgFile, 'w+' ) or die(
-			__(
-				"ERROR_CANNOT_CREATE_FILE",
-				"USER_CONFIG",
-				[ "cfgFilePath" => $this->cfgFile ]
-			)
-			);
-			if ( !fwrite( $fh, "<?php return $config ; ?>" ) ) {
-				die( "COULD NOT WRITE IN CONFIG FILE" );
-			}
-			fclose( $fh );
+			file_put_contents( $this->cfgFile, "<?php return $config ; ?>" );
 			
 			
 			return TRUE;
