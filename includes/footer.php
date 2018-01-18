b9e6bdca46c4456f4a4d9fe885ae559a8dcad2e6
reloxx13 <reloxx@interia.pl>
2018-01-19 00:13:31 +0100

    add tasmota git to footer

--- includes/footer.php
+++ includes/footer.php
@@ -1,10 +1,15 @@
 <div class='clear'></div>
 <div id='footer'>
-	&copy; <?php echo date( "Y" ); ?> <?php echo __( "BY" ); ?> reloxx13 - rev<?php echo $Config->read(
+	&copy; <?php echo date( "Y" ); ?> <?php echo __( "BY" ); ?> reloxx13 - rev <?php echo $Config->read(
 		"current_git_sha"
 	); ?> -
 	<a href='https://github.com/reloxx13/SonWEB' title='SonWEB <?php echo __( "VIEW_ON_GITHUB" ); ?>' target='_blank'>
-		<?php echo __( "VIEW_ON_GITHUB" ); ?>
+		SonWEB GitHub<?php //echo __( "VIEW_ON_GITHUB" ); ?>
+	</a> -
+	<a href='https://github.com/arendst/Sonoff-Tasmota'
+	   title='Tasmota <?php echo __( "VIEW_ON_GITHUB" ); ?>'
+	   target='_blank'>
+		Tasmota GitHub<?php //echo __( "VIEW_ON_GITHUB" ); ?>
 	</a>
 </div>
 </body>
