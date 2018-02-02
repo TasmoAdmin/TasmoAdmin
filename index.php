7289a9727b4eb4e3ae9da02c5063206a71e54ce5
reloxx13 <reloxx@interia.pl>
2018-02-02 22:27:11 +0100

    fix friendly url subdir

--- index.php
+++ index.php
@@ -50,45 +50,45 @@
 
 
 <div id="navi" class='open desktop'>
-    <ul>
-        <a href='<?php echo _BASEURL_; ?>start' title='<?php echo __( "STARTPAGE_TOOLTIP", "NAVI" ); ?>'>
-            <li class=''><?php echo __( "STARTPAGE", "NAVI" ); ?></li>
-        </a>
-        <hr/>
-        <a href='<?php echo _BASEURL_; ?>devices' title='<?php echo __( "DEVICES_TOOLTIP", "NAVI" ); ?>'>
-            <li class=''><?php echo __( "DEVICES", "NAVI" ); ?></li>
-        </a>
-        <a href='<?php echo _BASEURL_; ?>upload_form' class=''
-           title='<?php echo __( "UPDATE_TOOLTIP", "NAVI" ); ?>'>
-            <li class=''><?php echo __( "UPDATE", "NAVI" ); ?></li>
-        </a>
-
-        <!--			<hr/>-->
-        <!--			<a href='<?php echo _APPROOT_; ?>index.php?page=chat' title='Chat'>-->
-        <!--				<li class=''>Chat</li>-->
-        <!--			</a>-->
-        <hr/>
-        <a href='<?php echo _BASEURL_; ?>site_config'
-           title='<?php echo __( "SETTINGS_TOOLTIP", "NAVI" ); ?>'>
-            <li class=''><?php echo __( "SETTINGS", "NAVI" ); ?></li>
-        </a>
-        <a href='<?php echo _BASEURL_; ?>selfupdate'
-           title='<?php echo __( "SELFUPDATE_TOOLTIP", "NAVI" ); ?>'>
-            <li class=''><?php echo __( "SELFUPDATE", "NAVI" ); ?></li>
-        </a>
-        <hr/>
-        <a href='<?php echo _BASEURL_; ?>logout' title='<?php echo __( "LOGOUT_TOOLTIP", "NAVI" ); ?>'>
-            <li class=''><?php echo __( "LOGOUT", "NAVI" ); ?></li>
-        </a>
-
-    </ul>
+	<ul>
+		<a href='<?php echo _BASEURL_; ?>start' title='<?php echo __( "STARTPAGE_TOOLTIP", "NAVI" ); ?>'>
+			<li class=''><?php echo __( "STARTPAGE", "NAVI" ); ?></li>
+		</a>
+		<hr/>
+		<a href='<?php echo _BASEURL_; ?>devices' title='<?php echo __( "DEVICES_TOOLTIP", "NAVI" ); ?>'>
+			<li class=''><?php echo __( "DEVICES", "NAVI" ); ?></li>
+		</a>
+		<a href='<?php echo _BASEURL_; ?>upload_form' class=''
+		   title='<?php echo __( "UPDATE_TOOLTIP", "NAVI" ); ?>'>
+			<li class=''><?php echo __( "UPDATE", "NAVI" ); ?></li>
+		</a>
+		
+		<!--			<hr/>-->
+		<!--			<a href='<?php echo _APPROOT_; ?>index.php?page=chat' title='Chat'>-->
+		<!--				<li class=''>Chat</li>-->
+		<!--			</a>-->
+		<hr/>
+		<a href='<?php echo _BASEURL_; ?>site_config'
+		   title='<?php echo __( "SETTINGS_TOOLTIP", "NAVI" ); ?>'>
+			<li class=''><?php echo __( "SETTINGS", "NAVI" ); ?></li>
+		</a>
+		<a href='<?php echo _BASEURL_; ?>selfupdate'
+		   title='<?php echo __( "SELFUPDATE_TOOLTIP", "NAVI" ); ?>'>
+			<li class=''><?php echo __( "SELFUPDATE", "NAVI" ); ?></li>
+		</a>
+		<hr/>
+		<a href='<?php echo _BASEURL_; ?>logout' title='<?php echo __( "LOGOUT_TOOLTIP", "NAVI" ); ?>'>
+			<li class=''><?php echo __( "LOGOUT", "NAVI" ); ?></li>
+		</a>
+	
+	</ul>
 </div>
 
 
 <div id="content-holder" class='<?php echo $page == "start" ? "full-width" : ""; ?>'>
-    <div id='content' data-refreshtime='<?php echo $Config->read( "refreshtime" ); ?>'>
+	<div id='content' data-refreshtime='<?php echo $Config->read( "refreshtime" ); ?>'>
 		<?php include_once( _PAGESDIR_.$page.".php" ); ?>
-    </div>
+	</div>
 </div>
 <?php include_once( _INCLUDESDIR_."footer.php" ); //always load header?>
 
