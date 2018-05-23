7bf8cd2948fc30a521b0cb18903b422fe6a82a79
raymondmouthaan <raymondmmouthaan@gmail.com>
2018-05-23 22:37:25 +0200

    rename to TasmoAdmin

--- .docker/docker.sh
+++ .docker/docker.sh
@@ -39,52 +39,52 @@ docker_prepare() {
 
 docker_build() {
     echo "DOCKER BUILD: Build all docker images."
-    docker build --build-arg BASE_IMAGE=amd64/alpine --build-arg SONWEB_VERSION=$SONWEB_VERSION --build-arg QEMU_ARCH=x86_64 --file ./.docker/Dockerfile.alpine-tmpl --tag $IMAGE:build-$SONWEB_VERSION-alpine-amd64 .
-    docker build --build-arg BASE_IMAGE=arm32v6/alpine --build-arg SONWEB_VERSION=$SONWEB_VERSION --build-arg QEMU_ARCH=arm --file ./.docker/Dockerfile.alpine-tmpl --tag $IMAGE:build-$SONWEB_VERSION-alpine-arm32v6 .
-    docker build --build-arg BASE_IMAGE=arm64v8/alpine --build-arg SONWEB_VERSION=$SONWEB_VERSION --build-arg QEMU_ARCH=aarch64 --file ./.docker/Dockerfile.alpine-tmpl --tag $IMAGE:build-$SONWEB_VERSION-alpine-arm64v8 .
+    docker build --build-arg BASE_IMAGE=amd64/alpine --build-arg TASMOADMIN_VERSION=$TASMOADMIN_VERSION --build-arg QEMU_ARCH=x86_64 --file ./.docker/Dockerfile.alpine-tmpl --tag $IMAGE:build-$TASMOADMIN_VERSION-alpine-amd64 .
+    docker build --build-arg BASE_IMAGE=arm32v6/alpine --build-arg TASMOADMIN_VERSION=$TASMOADMIN_VERSION --build-arg QEMU_ARCH=arm --file ./.docker/Dockerfile.alpine-tmpl --tag $IMAGE:build-$TASMOADMIN_VERSION-alpine-arm32v6 .
+    docker build --build-arg BASE_IMAGE=arm64v8/alpine --build-arg TASMOADMIN_VERSION=$TASMOADMIN_VERSION --build-arg QEMU_ARCH=aarch64 --file ./.docker/Dockerfile.alpine-tmpl --tag $IMAGE:build-$TASMOADMIN_VERSION-alpine-arm64v8 .
 }
 
 docker_test() {
     echo "DOCKER TEST: Test all docker images."
-    docker run -d --name=test-$SONWEB_VERSION-alpine-amd64 $IMAGE:build-$SONWEB_VERSION-alpine-amd64
+    docker run -d --name=test-$TASMOADMIN_VERSION-alpine-amd64 $IMAGE:build-$TASMOADMIN_VERSION-alpine-amd64
     if [ $? -ne 0 ]; then
-        echo "ERROR: Docker container failed to start for build-$SONWEB_VERSION-alpine-amd64 ."
+        echo "ERROR: Docker container failed to start for build-$TASMOADMIN_VERSION-alpine-amd64 ."
         exit 1
     fi
-    docker stop test-$SONWEB_VERSION-alpine-amd64 && docker rm test-$SONWEB_VERSION-alpine-amd64
+    docker stop test-$TASMOADMIN_VERSION-alpine-amd64 && docker rm test-$TASMOADMIN_VERSION-alpine-amd64
 
-    docker run -d --name=test-$SONWEB_VERSION-alpine-arm32v6 $IMAGE:build-$SONWEB_VERSION-alpine-arm32v6
+    docker run -d --name=test-$TASMOADMIN_VERSION-alpine-arm32v6 $IMAGE:build-$TASMOADMIN_VERSION-alpine-arm32v6
     if [ $? -ne 0 ]; then
-        echo "ERROR: Docker container failed to start for build-$SONWEB_VERSION-alpine-arm32v6 ."
+        echo "ERROR: Docker container failed to start for build-$TASMOADMIN_VERSION-alpine-arm32v6 ."
         exit 1
     fi
-    docker stop test-$SONWEB_VERSION-alpine-arm32v6 && docker rm test-$SONWEB_VERSION-alpine-arm32v6
+    docker stop test-$TASMOADMIN_VERSION-alpine-arm32v6 && docker rm test-$TASMOADMIN_VERSION-alpine-arm32v6
 
-    docker run -d --name=test-$SONWEB_VERSION-alpine-arm64v8 $IMAGE:build-$SONWEB_VERSION-alpine-arm64v8
+    docker run -d --name=test-$TASMOADMIN_VERSION-alpine-arm64v8 $IMAGE:build-$TASMOADMIN_VERSION-alpine-arm64v8
     if [ $? -ne 0 ]; then
-        echo "ERROR: Docker container failed to start for build-$SONWEB_VERSION-alpine-arm64v8 ."
+        echo "ERROR: Docker container failed to start for build-$TASMOADMIN_VERSION-alpine-arm64v8 ."
         exit 1
     fi
-    docker stop test-$SONWEB_VERSION-alpine-arm64v8 && docker rm test-$SONWEB_VERSION-alpine-arm64v8
+    docker stop test-$TASMOADMIN_VERSION-alpine-arm64v8 && docker rm test-$TASMOADMIN_VERSION-alpine-arm64v8
 }
 
 docker_tag() {
     echo "DOCKER TAG: Tag all docker images."
-    docker tag $IMAGE:build-$SONWEB_VERSION-alpine-amd64 $IMAGE:$SONWEB_VERSION-alpine-amd64
-    docker tag $IMAGE:build-$SONWEB_VERSION-alpine-amd64 $IMAGE:latest-alpine-amd64
+    docker tag $IMAGE:build-$TASMOADMIN_VERSION-alpine-amd64 $IMAGE:$TASMOADMIN_VERSION-alpine-amd64
+    docker tag $IMAGE:build-$TASMOADMIN_VERSION-alpine-amd64 $IMAGE:latest-alpine-amd64
 
-    docker tag $IMAGE:build-$SONWEB_VERSION-alpine-arm32v6 $IMAGE:$SONWEB_VERSION-alpine-arm32v6
-    docker tag $IMAGE:build-$SONWEB_VERSION-alpine-arm32v6 $IMAGE:latest-alpine-arm32v6
+    docker tag $IMAGE:build-$TASMOADMIN_VERSION-alpine-arm32v6 $IMAGE:$TASMOADMIN_VERSION-alpine-arm32v6
+    docker tag $IMAGE:build-$TASMOADMIN_VERSION-alpine-arm32v6 $IMAGE:latest-alpine-arm32v6
 
-    docker tag $IMAGE:build-$SONWEB_VERSION-alpine-arm64v8 $IMAGE:$SONWEB_VERSION-alpine-arm64v8
-    docker tag $IMAGE:build-$SONWEB_VERSION-alpine-arm64v8 $IMAGE:latest-alpine-arm64v8
+    docker tag $IMAGE:build-$TASMOADMIN_VERSION-alpine-arm64v8 $IMAGE:$TASMOADMIN_VERSION-alpine-arm64v8
+    docker tag $IMAGE:build-$TASMOADMIN_VERSION-alpine-arm64v8 $IMAGE:latest-alpine-arm64v8
 }
 
 docker_push() {
     echo "DOCKER PUSH: Push all docker images."
-    docker push $IMAGE:$SONWEB_VERSION-alpine-amd64
-    docker push $IMAGE:$SONWEB_VERSION-alpine-arm32v6
-    docker push $IMAGE:$SONWEB_VERSION-alpine-arm64v8
+    docker push $IMAGE:$TASMOADMIN_VERSION-alpine-amd64
+    docker push $IMAGE:$TASMOADMIN_VERSION-alpine-arm32v6
+    docker push $IMAGE:$TASMOADMIN_VERSION-alpine-arm64v8
 }
 
 docker_manifest_list() {
@@ -96,30 +96,30 @@ docker_manifest_list() {
 }
 
 docker_manifest_list_version() {
-    # Manifest Create $SONWEB_VERSION default
-    docker manifest create $IMAGE:$SONWEB_VERSION \
-        $IMAGE:$SONWEB_VERSION-alpine-amd64 \
-        $IMAGE:$SONWEB_VERSION-alpine-arm32v6 \
-        $IMAGE:$SONWEB_VERSION-alpine-arm64v8
+    # Manifest Create $TASMOADMIN_VERSION default
+    docker manifest create $IMAGE:$TASMOADMIN_VERSION \
+        $IMAGE:$TASMOADMIN_VERSION-alpine-amd64 \
+        $IMAGE:$TASMOADMIN_VERSION-alpine-arm32v6 \
+        $IMAGE:$TASMOADMIN_VERSION-alpine-arm64v8
 
-    # Manifest Annotate SONWEB_VERSION
-    docker manifest annotate $IMAGE:$SONWEB_VERSION $IMAGE:$SONWEB_VERSION-alpine-arm32v6 --os=linux --arch=arm --variant=v6
-    docker manifest annotate $IMAGE:$SONWEB_VERSION $IMAGE:$SONWEB_VERSION-alpine-arm64v8 --os=linux --arch=arm64 --variant=v8
+    # Manifest Annotate TASMOADMIN_VERSION
+    docker manifest annotate $IMAGE:$TASMOADMIN_VERSION $IMAGE:$TASMOADMIN_VERSION-alpine-arm32v6 --os=linux --arch=arm --variant=v6
+    docker manifest annotate $IMAGE:$TASMOADMIN_VERSION $IMAGE:$TASMOADMIN_VERSION-alpine-arm64v8 --os=linux --arch=arm64 --variant=v8
 
-    # Manifest Push SONWEB_VERSION
-    docker manifest push $IMAGE:$SONWEB_VERSION
+    # Manifest Push TASMOADMIN_VERSION
+    docker manifest push $IMAGE:$TASMOADMIN_VERSION
 }
 
 docker_manifest_list_latest() {
     # Manifest Create LATEST
     docker manifest create $IMAGE:latest \
-        $IMAGE:$SONWEB_VERSION-alpine-amd64 \
-        $IMAGE:$SONWEB_VERSION-alpine-arm32v6 \
-        $IMAGE:$SONWEB_VERSION-alpine-arm64v8
+        $IMAGE:$TASMOADMIN_VERSION-alpine-amd64 \
+        $IMAGE:$TASMOADMIN_VERSION-alpine-arm32v6 \
+        $IMAGE:$TASMOADMIN_VERSION-alpine-arm64v8
 
     # Manifest Annotate LATEST
-    docker manifest annotate $IMAGE:latest $IMAGE:$SONWEB_VERSION-alpine-arm32v6 --os=linux --arch=arm --variant=v6
-    docker manifest annotate $IMAGE:latest $IMAGE:$SONWEB_VERSION-alpine-arm64v8 --os=linux --arch=arm64 --variant=v8
+    docker manifest annotate $IMAGE:latest $IMAGE:$TASMOADMIN_VERSION-alpine-arm32v6 --os=linux --arch=arm --variant=v6
+    docker manifest annotate $IMAGE:latest $IMAGE:$TASMOADMIN_VERSION-alpine-arm64v8 --os=linux --arch=arm64 --variant=v8
 
     # Manifest Push LATEST
     docker manifest push $IMAGE:latest
@@ -127,26 +127,26 @@ docker_manifest_list_latest() {
 
 docker_manifest_list_version_os_arch(){
     # Manifest Create alpine-amd64
-    docker manifest create $IMAGE:$SONWEB_VERSION-alpine-amd64 \
-        $IMAGE:$SONWEB_VERSION-alpine-amd64
+    docker manifest create $IMAGE:$TASMOADMIN_VERSION-alpine-amd64 \
+        $IMAGE:$TASMOADMIN_VERSION-alpine-amd64
 
-    docker manifest push $IMAGE:$SONWEB_VERSION-alpine-amd64
+    docker manifest push $IMAGE:$TASMOADMIN_VERSION-alpine-amd64
 
     # Manifest Create alpine-arm32v6
-    docker manifest create $IMAGE:$SONWEB_VERSION-alpine-arm32v6 \
-        $IMAGE:$SONWEB_VERSION-alpine-arm32v6
+    docker manifest create $IMAGE:$TASMOADMIN_VERSION-alpine-arm32v6 \
+        $IMAGE:$TASMOADMIN_VERSION-alpine-arm32v6
 
-    docker manifest annotate $IMAGE:$SONWEB_VERSION-alpine-arm32v6 $IMAGE:$SONWEB_VERSION-alpine-arm32v6 --os=linux --arch=arm --variant=v6
+    docker manifest annotate $IMAGE:$TASMOADMIN_VERSION-alpine-arm32v6 $IMAGE:$TASMOADMIN_VERSION-alpine-arm32v6 --os=linux --arch=arm --variant=v6
 
-    docker manifest push $IMAGE:$SONWEB_VERSION-alpine-arm32v6
+    docker manifest push $IMAGE:$TASMOADMIN_VERSION-alpine-arm32v6
 
     # Manifest Create alpine-arm64v8
-    docker manifest create $IMAGE:$SONWEB_VERSION-alpine-arm64v8 \
-        $IMAGE:$SONWEB_VERSION-alpine-arm64v8
+    docker manifest create $IMAGE:$TASMOADMIN_VERSION-alpine-arm64v8 \
+        $IMAGE:$TASMOADMIN_VERSION-alpine-arm64v8
 
-    docker manifest annotate $IMAGE:$SONWEB_VERSION-alpine-arm64v8 $IMAGE:$SONWEB_VERSION-alpine-arm64v8 --os=linux --arch=arm64 --variant=v8
+    docker manifest annotate $IMAGE:$TASMOADMIN_VERSION-alpine-arm64v8 $IMAGE:$TASMOADMIN_VERSION-alpine-arm64v8 --os=linux --arch=arm64 --variant=v8
 
-    docker manifest push $IMAGE:$SONWEB_VERSION-alpine-arm64v8
+    docker manifest push $IMAGE:$TASMOADMIN_VERSION-alpine-arm64v8
 }
 
 setup_dependencies() {
