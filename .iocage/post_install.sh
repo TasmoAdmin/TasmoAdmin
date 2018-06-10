ff66c8686c47d6e52dfd40eb65ccd31e50fb781e
Troy Prelog <35702532+tprelog@users.noreply.github.com>
2018-06-10 14:18:25 -0400

    initial commit
    Former-commit-id: efe76d47858550a818d6cdde6ea93a3ce5a78986

---
+++ .iocage/post_install.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# pkg install git nginx php72 php72-zip php72-curl php72-json php72-session php72-filter
+
+git clone https://github.com/tprelog/TasmoAdmin.git /root/TasmoAdmin
+cp -R /root/TasmoAdmin/tasmoadmin /usr/local/www/tasmoadmin
+
+find /usr/local/www/tasmoadmin -type f -name ".htaccess" -depth -exec rm -f {} \;
+find /usr/local/www/tasmoadmin -type f -name ".empty" -depth -exec rm -f {} \;
+
+cp /usr/local/etc/nginx/nginx.conf /usr/local/etc/nginx/nginx.conf~
+cp /root/TasmoAdmin/.iocage/overlay/usr/local/etc/nginx/nginx.conf /usr/local/etc/nginx/nginx.conf
+
+cp /usr/local/etc/php-fpm.d/www.conf /usr/local/etc/php-fpm.d/www.conf~
+cp /root/TasmoAdmin/.iocage/overlay/usr/local/etc/php-fpm.d/www.conf /usr/local/etc/php-fpm.d/www.conf
+
+chown -R www:www /usr/local/www/tasmoadmin
+
+sysrc -f /etc/rc.conf nginx_enable=YES
+sysrc -f /etc/rc.conf php_fpm_enable=YES
+
+service nginx start  2>/dev/null
+service php-fpm start  2>/dev/null
