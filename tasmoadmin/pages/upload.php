8d935626bb7116e1e9d59b54c22128ff3d805759
Malachi Soord <inverse.chi@gmail.com>
2022-01-06 22:13:05 +0100

    Fix loading versions by passing user-agent (#458)
    * Fix loading versions by passing user-agent
    
    * Set consistent UA
    
    Former-commit-id: 2e99eea9ed2e390517ef4470fe6fed21bd696e52

--- tasmoadmin/pages/upload.php
+++ tasmoadmin/pages/upload.php
@@ -180,11 +180,7 @@ elseif (isset($_REQUEST["auto"])) {
 		$ch = curl_init();
 		curl_setopt($ch, CURLOPT_URL, $url);
 		curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-		curl_setopt(
-			$ch,
-			CURLOPT_USERAGENT,
-			'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.8.1.13) Gecko/20080311 Firefox/2.0.0.13'
-		);
+		curl_setopt($ch, CURLOPT_USERAGENT, "TasmoAdmin/{$Config->read('current_git_tag')}" );
 		$result = curl_exec($ch);
 		curl_close($ch);
 		
