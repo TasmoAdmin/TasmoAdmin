1cca7c4265bcec9e2d6a7445272a7b39d5b09303
Malachi Soord <inverse.chi@gmail.com>
2022-04-15 22:56:27 +0200

    More tests

--- tasmoadmin/tests/Helper/LoginHelperTest.php
+++ tasmoadmin/tests/Helper/LoginHelperTest.php
@@ -8,10 +8,43 @@ use TasmoAdmin\Helper\LoginHelper;
 
 class LoginHelperTest extends TestCase
 {
-    public function testRegisterTest() {
+    public function testRegister(): void
+    {
         $configMock = $this->createMock(Config::class);
         $configMock->expects($this->exactly(2))->method('write');
         $loginHelper = new LoginHelper($configMock);
         $loginHelper->register('test-user', 'test-password');
+    }
+
+    public function testLoginValid(): void
+    {
+        $configMock = $this->createMock(Config::class);
+        $loginHelper = new LoginHelper($configMock);
+
+        $password = 'top-secret';
+        $storedPassword = password_hash($password, PASSWORD_DEFAULT);
+
+        self::assertTrue($loginHelper->login($password, $storedPassword));
+    }
+
+    public function testLoginInvalid(): void
+    {
+        $configMock = $this->createMock(Config::class);
+        $loginHelper = new LoginHelper($configMock);
+
+        $password = 'wrong-password';
+        $storedPassword = password_hash('top-secret', PASSWORD_DEFAULT);
+
+        self::assertFalse($loginHelper->login($password, $storedPassword));
+    }
+
+    public function testLoginUpgrade(): void
+    {
+        $configMock = $this->createMock(Config::class);
+        $loginHelper = new LoginHelper($configMock);
+        $password = 'top-secret';
+        $storedPassword = md5($password);
+        $configMock->expects($this->once())->method('write');
+        self::assertTrue($loginHelper->login($password, $storedPassword));
     }
 }
