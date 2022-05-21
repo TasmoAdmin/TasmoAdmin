4e60b492692d57d7bb293dc4833155edab79c5c1
Malachi Soord <inverse.chi@gmail.com>
2022-05-21 22:07:13 +0200

    Revert "Refactor update work (#532)"
    This reverts commit 8fe1b4ae60ddc3ba74b663c57a5623b0b7993846.

--- tasmoadmin/tests/TestUtils.php
+++ tasmoadmin/tests/TestUtils.php
@@ -8,9 +8,4 @@ class TestUtils
     {
         return FIXTURE_PATH . $path;
     }
-
-    public static function loadFixture(string $path): string
-    {
-        return file_get_contents(self::getFixturePath($path));
-    }
 }
