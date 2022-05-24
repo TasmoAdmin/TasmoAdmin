abe0464f87ae035b28f8d686c22d00e6147a4387
Malachi Soord <inverse.chi@gmail.com>
2022-05-24 23:43:07 +0200

    Rework + tests

--- tasmoadmin/tests/bootstrap.php
+++ tasmoadmin/tests/bootstrap.php
@@ -10,3 +10,12 @@ const _CSVFILE_ = "";
 const _TMPDIR_ = "";
 
 const FIXTURE_PATH = __DIR__ . '/fixtures/';
+
+function __(string $string, ?string $category = null, array $args = []): string
+{
+    $cat = "";
+    if( isset( $category ) && !empty( $category ) ) {
+        $cat = $category."_";
+    }
+    return sprintf('%s%s: %s', $cat, $string, implode(',', $args));
+}
