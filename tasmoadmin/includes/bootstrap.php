e4d5b8af289a191344104b2908fb388343886499
Malachi Soord <inverse.chi@gmail.com>
2022-06-12 22:31:58 +0200

    Fix urlencode (#559)

--- tasmoadmin/includes/bootstrap.php
+++ tasmoadmin/includes/bootstrap.php
@@ -152,7 +152,7 @@ if( isset( $_GET ) ) {
         if( isset( $_REQUEST[ "target" ] ) ) {
             $data = $Sonoff->setDeviceValue( $_REQUEST[ "id" ], $_REQUEST[ "field" ], $_REQUEST[ "newvalue" ] );
         } else {
-            $data = $Sonoff->doAjax($_REQUEST["id"], $_REQUEST['cmnd']);
+            $data = $Sonoff->doAjax($_REQUEST["id"], urldecode($_REQUEST['cmnd']));
         }
         header( 'Content-Type: application/json' );
         echo json_encode( $data );
