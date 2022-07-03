58d356961cd06f92c5db3319a63f54a59a2fae26
Malachi Soord <inverse.chi@gmail.com>
2022-07-03 21:36:37 +0200

    Correct changelog resolution (#584)
    * Fix changelog
    
    * Use single source

--- tasmoadmin/src/Helper/TasmotaHelper.php
+++ tasmoadmin/src/Helper/TasmotaHelper.php
@@ -23,7 +23,6 @@ class TasmotaHelper
     public function getReleaseNotes(): string
     {
         $releaseLog = $this->getContents('https://raw.githubusercontent.com/arendst/Tasmota/development/RELEASENOTES.md');
-
         $releaseLog = str_replace(["*/", "/*", " *\n"], ["", "", ""], $releaseLog);
         $releaseLog = str_replace("https://github.com/arendst/Tasmota/blob/master/tools/logo/TASMOTA_FullLogo_Vector.svg",
             "https://raw.githubusercontent.com/arendst/Tasmota/master/tools/logo/TASMOTA_FullLogo_Vector.svg",
@@ -40,15 +39,8 @@ class TasmotaHelper
     public function getChangelog(): string
     {
         $changeLog = $this->getContents('https://raw.githubusercontent.com/arendst/Tasmota/master/CHANGELOG.md');
-        $changeLog .= $this->getContents('https://raw.githubusercontent.com/tasmota/docs/master/docs/changelog.md');
         $changeLog = $this->markDownParser->parse($changeLog);
         $changeLog = $this->replaceIssuesWithUrls($changeLog);
-        $changeLog = str_replace(
-            ":rotating_light:",
-            //		"<i class=\"error red fas fa-exclamation-triangle\" style='color: red;'></i>",
-            "<img alt=\"🚨\" class=\"emojione\" src=\"https://cdnjs.cloudflare.com/ajax/libs/emojione/2.2.7/assets/png/1f6a8.png\" title=\":rotating_light:\">",
-            $changeLog
-        );
 
         return $changeLog;
     }
