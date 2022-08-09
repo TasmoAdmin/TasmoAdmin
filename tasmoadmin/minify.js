06d6c4934f22954f1d438e20299b1845045b295b
Malachi Soord <inverse.chi@gmail.com>
2022-08-09 22:05:42 +0200

    Add css minification (#620)

--- tasmoadmin/minify.js
+++ tasmoadmin/minify.js
@@ -1,6 +1,7 @@
 const glob = require('glob');
 const minify = require('@node-minify/core');
 const terser = require('@node-minify/terser');
+const cleanCSS = require('@node-minify/clean-css');
 
 glob('resources/js/*.js', (err, files) => {
     if (err) {
@@ -17,3 +18,20 @@ glob('resources/js/*.js', (err, files) => {
         });
     })
 });
+
+glob('resources/css/*.css', (err, files) => {
+    if (err) {
+        console.log(err);
+        return;
+    }
+
+    files.forEach((file) => {
+        console.log(`processing ${file}`);
+        minify({
+            compressor: cleanCSS,
+            input: file,
+            output: file.slice(0, -4) + '.min.css',
+        });
+    })
+});
+
