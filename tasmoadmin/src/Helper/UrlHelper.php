4b8e691780c1801e73a64304c532c06925626bb2
Malachi Soord <inverse.chi@gmail.com>
2022-09-06 22:17:52 +0200

    Fix URLHelper for min

--- tasmoadmin/src/Helper/UrlHelper.php
+++ tasmoadmin/src/Helper/UrlHelper.php
@@ -6,41 +6,55 @@ use TasmoAdmin\Config;
 
 class UrlHelper
 {
-    private Config $config;
-
     private string $baseUrl;
 
     private string $resourceUrl;
 
-    public function __construct(Config $config,  string $baseUrl, string $resourceUrl)
+    private string $resourceDir;
+
+    private bool $minimizeResources;
+
+    private ?string $currentGitTag;
+
+    public function __construct(Config $config,  string $baseUrl, string $resourceUrl, string $resourceDir)
     {
-        $this->config = $config;
         $this->baseUrl = $baseUrl;
         $this->resourceUrl = $resourceUrl;
+        $this->resourceDir = $resourceDir;
+        $this->minimizeResources = $config->read("minimize_resources") === "1";
+        $this->currentGitTag = $config->read("current_git_tag");
+    }
+
+    private function getCacheTag(): string
+    {
+        $cacheTag = $this->currentGitTag;
+        if (empty($cacheTag)) {
+            $cacheTag = time();
+        }
+
+        $cacheTag = str_replace(".", "", $cacheTag);
+
+        return "?_=" . $cacheTag;
     }
 
     public function style(string $filename, ?string $csspath = null): string
     {
         if ($csspath === null) {
             $csspath = $this->resourceUrl . "css/";
+            $cssReal = $this->resourceDir . "css/";
         } else {
             $csspath = $this->baseUrl . $csspath;
+            $cssReal = $this->baseUrl . $csspath;
         }
 
-        $cacheTag = time();
+        $cacheTag = $this->getCacheTag();
         $min = "";
-        if ($this->config->read("minimize_resources") === "1") {
-            $cacheTag = $this->config->read("current_git_tag");
-            if (empty($cacheTag)) {
-                $cacheTag = time();
-            }
+        if ($this->minimizeResources) {
             $min = ".min";
         }
-        $cacheTag = str_replace(".", "", $cacheTag);
-        $cacheTag = "?_=" . $cacheTag;
 
         $path = $filename . $min . ".css";
-        if (file_exists($csspath . $path)) {
+        if (file_exists($cssReal . $path)) {
             $filepath = $csspath . $path . $cacheTag;
         } else {
             $filepath = $csspath . $filename . ".css" . $cacheTag;
@@ -53,24 +67,20 @@ class UrlHelper
     {
         if ($jspath === null) {
             $jspath = $this->resourceUrl . "js/";
+            $jsReal = $this->resourceDir . "js/";
         } else {
             $jspath = $this->baseUrl . $jspath;
+            $jsReal = $this->baseUrl . $jspath;
         }
 
-        $cacheTag = time();
+        $cacheTag = $this->getCacheTag();
         $min = "";
-        if ($this->config->read("minimize_resources") === "1") {
-            $cacheTag = $this->config->read("current_git_tag");
-            if (empty($cacheTag)) {
-                $cacheTag = time();
-            }
+        if ($this->minimizeResources) {
             $min = ".min";
         }
-        $cacheTag = str_replace(".", "", $cacheTag);
-        $cacheTag = "?_=" . $cacheTag;
 
         $path = $filename . $min . ".js";
-        if (file_exists($jspath . $path)) {
+        if (file_exists($jsReal . $path)) {
             $filepath = $jspath . $path . $cacheTag;
         } else {
             $filepath = $jspath . $filename . ".js" . $cacheTag;
