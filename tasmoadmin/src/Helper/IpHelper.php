fa5394101e6ecccb82544534b7d338057fa595be
Malachi Soord <inverse.chi@gmail.com>
2022-10-06 21:05:02 +0200

    Skip using range

--- tasmoadmin/src/Helper/IpHelper.php
+++ tasmoadmin/src/Helper/IpHelper.php
@@ -17,19 +17,22 @@ class IpHelper
             throw new InvalidArgumentException(sprintf('%s is an invalid IPv4 address', $toIp));
         }
 
+        $fromIpLong = ip2long($fromIp);
+        $toIpLong = ip2long($toIp);
+
+
+        if (abs($fromIpLong - $toIpLong) > self::MAX_IPS) {
+            throw new InvalidArgumentException('The defined IP range is too large, please specify a smaller range');
+        }
+
         $ips = [];
-        foreach ($this->xrange(ip2long($fromIp), ip2long($toIp)) as $ip) {
+        foreach (range(ip2long($fromIp), ip2long($toIp)) as $ip) {
             $ip = long2ip($ip);
-            if (in_array($ip, $excludedIps)) {
+            if (in_array($ip, $excludedIps, true)) {
                 continue;
             }
     
             $ips[] = $ip;
-
-            if (count($ips) > self::MAX_IPS) {
-                throw new InvalidArgumentException('The defined IP range is too large, please specify a smaller range');
-            }
-
         }
 
         return $ips;
@@ -40,25 +43,4 @@ class IpHelper
     {
         return ip2long($ip) !== false;
     }
-
-    private function xrange(int $start, int $limit, int $step = 1) {
-        if ($start <= $limit) {
-            if ($step <= 0) {
-                throw new InvalidArgumentException('Step must be positive');
-            }
-    
-            for ($i = $start; $i <= $limit; $i += $step) {
-                yield $i;
-            }
-        } else {
-            if ($step >= 0) {
-                throw new InvalidArgumentException('Step must be negative');
-            }
-    
-            for ($i = $start; $i >= $limit; $i += $step) {
-                yield $i;
-            }
-        }
-    }
-
 }
