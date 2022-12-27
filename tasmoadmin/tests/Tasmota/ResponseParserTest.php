390ea08940681869d109ad2717c513deac4f85d8
Malachi Soord <inverse.chi@gmail.com>
2022-12-27 23:43:24 +0100

    Additional test (#738)

--- tasmoadmin/tests/Tasmota/ResponseParserTest.php
+++ tasmoadmin/tests/Tasmota/ResponseParserTest.php
@@ -8,12 +8,21 @@ use Tests\TasmoAdmin\TestUtils;
 
 class ResponseParserTest extends TestCase
 {
-    public function testProcessResultComaptability(): void
+    public function testProcessResultCompatability(): void
     {
         $parser = new ResponseParser();
 
         $result = $parser->processResult('{"StatusNET":{"IP": "IP_ADDRESS"}}');
 
         self::assertEquals("IP_ADDRESS", $result->StatusNET->IPAddress);
+    }
+
+    public function testProcessResultInvalid(): void
+    {
+        $parser = new ResponseParser();
+        $result = $parser->processResult('{"Invalid": "\xfc\xa1\xa1\xa1\xa1\xa1"}');
+
+        self::assertObjectHasAttribute('ERROR', $result);
+        self::assertStringContainsString('{"Invalid": "\xfc\xa1\xa1\xa1\xa1\xa1"}', $result->ERROR);
     }
 }
