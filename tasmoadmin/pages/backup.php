d163b61009cdae48266175ace51495ef0b11e9d7
Malachi Soord <inverse.chi@gmail.com>
2023-01-07 23:59:26 +0100

    Init controls

--- tasmoadmin/pages/backup.php
+++ tasmoadmin/pages/backup.php
@@ -16,6 +16,43 @@
 
 <div class='row justify-content-center'>
     <div class='col'>
+        <div class='form-row mb-3'>
+            <div class='offset-1 col-auto col col-auto'>
+                <button type='submit' class='btn btn-success' name='submit' value='submit'>
+                    <?php echo __("BTN_START_BACKUP", "DEVICE_UPDATE"); ?>
+                </button>
+            </div>
+            <div class='col col-auto'>
+                <div class="form-check pl-0">
+                    <input type="checkbox"
+                           class="form-check-input showmore d-none"
+                           id="showmore"
+                           name='showmore'
+                    >
+                    <label class="form-check-label  btn btn-secondary" for="showmore">
+                        <?php echo __("SHOW_MORE", "DEVICES"); ?>
+                    </label>
+                </div>
+            </div>
+            <?php if ($Config->read("show_search") == 1): ?>
+                <div class="col col-auto">
+                    <div class="form-group">
+                        <div class="input-group">
+                            <input type="text"
+                                   name="searchterm"
+                                   class='form-control device-search has-clearer'
+                                   placeholder="<?php echo __("FILTER", "DEVICES"); ?>"
+                            >
+                            <div class="input-group-append">
+										<span class="input-group-text">
+											<i class="fas fa-search"></i>
+										</span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            <?php endif; ?>
+        </div>
         <div class='table-responsive double-scroll'>
             <?php
             $deviceLinks = true;
