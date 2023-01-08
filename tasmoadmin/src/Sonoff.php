2bac28e9965c1e6692489d9c683bdc0515792f2f
Malachi Soord <inverse.chi@gmail.com>
2023-01-08 17:16:15 +0100

    Improve backup names to include names

--- tasmoadmin/src/Sonoff.php
+++ tasmoadmin/src/Sonoff.php
@@ -75,7 +75,7 @@ class Sonoff
     public function backup(Device $device, string $downloadPath): string
     {
         $url = $this->buildBasicAuthUrl($device, 'dl');
-        $downloadFilePath = $downloadPath . $device->id . '.dmp';
+        $downloadFilePath = $downloadPath . sprintf('%s-%s.dmp', $device->id, implode('-', $device->names));
         if (file_exists($downloadFilePath)) {
             unlink($downloadFilePath);
         }
