5bed1d8c247d2c05c8c2b7f5d9f5edd12e5df373
Malachi Soord <inverse.chi@gmail.com>
2023-02-16 22:52:34 +0100

    Tidy up

--- .docker/rootfs/etc/cont-init.d/13-persistent-data-nginx.sh
+++ .docker/rootfs/etc/cont-init.d/13-persistent-data-nginx.sh
@@ -8,23 +8,22 @@
 
 # Setup structure if it does not exists
 if [ ! -d "/data/nginx" ]; then
-    echo 'Data nginx directory not initialized, doing that now...'
-    mkdir -p /data/nginx/certs
-    cp -Rv /etc/nginx/nginx.conf /etc/nginx/nginx.conf.org
-    cp -Rv /etc/nginx/nginx.conf.org /data/nginx/nginx.conf.org
-    cp -Rv /data/nginx/nginx.conf.org /data/nginx/nginx.conf
-    cp -Rv /etc/nginx/nginx-ssl.conf /data/nginx/nginx-ssl.conf
-    cp -Rv /etc/nginx/certs/* /data/nginx/certs
+  echo 'Data nginx directory not initialized, doing that now...'
+  mkdir -p /data/nginx/certs
+  cp -Rv /etc/nginx/nginx.conf /etc/nginx/nginx.conf.org
+  cp -Rv /etc/nginx/nginx.conf.org /data/nginx/nginx.conf.org
+  cp -Rv /data/nginx/nginx.conf.org /data/nginx/nginx.conf
+  cp -Rv /etc/nginx/nginx-ssl.conf /data/nginx/nginx-ssl.conf
+  cp -Rv /etc/nginx/certs/* /data/nginx/certs
 fi
-
 
 if [[ -L "/data/nginx/nginx.conf" ]]; then
-	echo "ERROR: /data/nginx/nginx.conf is a symlink, try to download the default nginx.conf from github";
-	rm -rf /data/nginx/nginx.conf
-	wget -O /data/nginx/nginx.conf https://raw.githubusercontent.com/TasmoAdmin/TasmoAdmin/master/.docker/rootfs/etc/nginx/nginx.conf
-    cp -Rv /data/nginx/nginx.conf /etc/nginx/nginx.conf.org
+  echo "ERROR: /data/nginx/nginx.conf is a symlink, try to download the default nginx.conf from github";
+  rm -rf /data/nginx/nginx.conf
+  wget -O /data/nginx/nginx.conf https://raw.githubusercontent.com/TasmoAdmin/TasmoAdmin/master/.docker/rootfs/etc/nginx/nginx.conf
+  cp -Rv /data/nginx/nginx.conf /etc/nginx/nginx.conf.org
 fi
 
-echo 'Symlinking /etc/nginx.conf to persistent storage location...'
+echo 'Symlinking /etc/nginx.conf from persistent storage location...'
 rm -rf /etc/nginx/nginx.conf
 ln -s /data/nginx/nginx.conf /etc/nginx/nginx.conf
