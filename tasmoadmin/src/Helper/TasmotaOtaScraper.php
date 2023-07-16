09a4a3a43dfe107434ee1ab51525804fbade3d00
Malachi Soord <inverse.chi@gmail.com>
2023-07-16 13:52:11 +0200

    Add support for ESP32

--- tasmoadmin/src/Helper/TasmotaOtaScraper.php
+++ tasmoadmin/src/Helper/TasmotaOtaScraper.php
@@ -8,31 +8,65 @@ use Symfony\Component\DomCrawler\Crawler;
 
 class TasmotaOtaScraper
 {
+    public const ESP8266 = 'ESP8266';
+    public const ESP32= 'ESP32';
+
     private const OTA_URLS = [
-        'stable' => 'https://ota.tasmota.com/tasmota/release/',
-        'dev' => 'https://ota.tasmota.com/tasmota',
+        'stable' => [
+            self::ESP8266 => 'https://ota.tasmota.com/tasmota/release/',
+            self::ESP32 => 'https://ota.tasmota.com/tasmota32/release/',
+            ],
+        'dev' => [
+            self::ESP8266 => 'https://ota.tasmota.com/tasmota/',
+            self::ESP32 => 'https://ota.tasmota.com/tasmota32/',
+        ],
     ];
 
-    private string $url;
+    private const RULES = [
+      self::ESP8266 => [
+          'date_column' => 6,
+      ],
+        self::ESP32 => [
+            'date_column' => 4,
+        ],
+    ];
+
+    private string $updateChannel;
 
     private HttpBrowser $client;
 
     public function __construct(string $updateChannel, HttpBrowser $client)
     {
-        $this->url = self::OTA_URLS[$updateChannel];
+        $this->updateChannel = $updateChannel;
         $this->client = $client;
     }
 
-    public function getFirmware(): TasmotaFirmwareResult
+    public function getEsp8266Firmware(): TasmotaFirmwareResult
     {
-        $crawler = $this->client->request('GET', $this->url);
+        return $this->getFirmware(self::ESP8266);
+    }
+
+    public function getEsp32Firmware(): TasmotaFirmwareResult
+    {
+        return $this->getFirmware(self::ESP32);
+    }
+
+    private function getFirmware(string $type)
+    {
+        $crawler = $this->client->request('GET', self::OTA_URLS[$this->updateChannel][$type]);
 
         $firmwares =  $crawler->filter('table tr td:nth-child(2)')->each(function ($node) {
             return new TasmotaFirmware(basename($node->text()), $node->text());
         });
 
+        foreach ($firmwares as $index => $firmware) {
+            if (!str_starts_with($firmware->getUrl(), 'http')) {
+                unset($firmwares[$index]);
+            }
+        }
+
         $version = $this->getVersion($crawler);
-        $publishDate =  $this->getPublishDate($crawler);
+        $publishDate =  $this->getPublishDate($crawler, $type);
 
         return new TasmotaFirmwareResult($version, $publishDate, $firmwares);
     }
@@ -46,9 +80,11 @@ class TasmotaOtaScraper
         return $matches[1];
     }
 
-    private function getPublishDate(Crawler $crawler): DateTime
+    private function getPublishDate(Crawler $crawler, string $type): DateTime
     {
-        $dates = $crawler->filter('table tr td:nth-child(6)')->each(function ($node) {
+        $dateColumn = self::RULES[$type]['date_column'];
+
+        $dates = $crawler->filter("table tr td:nth-child($dateColumn)")->each(function ($node) {
             return $node->text();
         });
 
