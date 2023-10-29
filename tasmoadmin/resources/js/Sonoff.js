7f30843c594681fb6f97d8902b8a5132da9ebf1c
Malachi Soord <inverse.chi@gmail.com>
2023-10-29 11:23:07 +0100

    Fix ajax calls by passing base_url in (#1010)

--- tasmoadmin/resources/js/Sonoff.js
+++ tasmoadmin/resources/js/Sonoff.js
@@ -103,7 +103,7 @@ class Sonoff {
         var ip = ip || id;
         $.ajax({
             dataType: "json",
-            url: "/actions?doAjax",
+            url: `${this.options.base_url}actions?doAjax`,
             timeout: this.options.timeout * 1000,
             cache: false,
             type: "post",
@@ -145,7 +145,7 @@ class Sonoff {
         var timeout = timeout || this.options.timeout;
         $.ajax({
             dataType: "json",
-            url: "/actions?doAjaxAll",
+            url: `${this.options.base_url}actions?doAjaxAll`,
             timeout: timeout * 1000,
             cache: false,
             type: "post",
@@ -261,7 +261,7 @@ class Sonoff {
     setDeviceValue(id, field, newvalue, td) {
         $.ajax({
             dataType: "json",
-            url: "/actions?doAjax",
+            url: `${this.options.base_url}actions?doAjax`,
             timeout: this.options.timeout * 1000,
             cache: false,
             type: "post",
