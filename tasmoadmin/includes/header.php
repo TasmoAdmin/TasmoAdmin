6e94d6b96a63f49aaf02ef9d0fcade6a27db22ab
Malachi Soord <inverse.chi@gmail.com>
2024-01-04 22:05:17 +0100

    Replace favicon (#1079)

--- tasmoadmin/includes/header.php
+++ tasmoadmin/includes/header.php
@@ -22,57 +22,11 @@ $urlHelper = $container->get(UrlHelper::class);
 		Remove this if you use the .htaccess
 		-->
 		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1"/>
-		
-		<link rel="apple-touch-icon"
-			  sizes="57x57"
-			  href="<?php echo _RESOURCESURL_; ?>img/favicons/apple-icon-57x57.png"
-		>
-		<link rel="apple-touch-icon"
-			  sizes="60x60"
-			  href="<?php echo _RESOURCESURL_; ?>img/favicons/apple-icon-60x60.png"
-		>
-		<link rel="apple-touch-icon"
-			  sizes="72x72"
-			  href="<?php echo _RESOURCESURL_; ?>img/favicons/apple-icon-72x72.png"
-		>
-		<link rel="apple-touch-icon"
-			  sizes="76x76"
-			  href="<?php echo _RESOURCESURL_; ?>img/favicons/apple-icon-76x76.png"
-		>
-		<link rel="apple-touch-icon" sizes="114x114"
-			  href="<?php echo _RESOURCESURL_; ?>img/favicons/apple-icon-114x114.png"
-		>
-		<link rel="apple-touch-icon" sizes="120x120"
-			  href="<?php echo _RESOURCESURL_; ?>img/favicons/apple-icon-120x120.png"
-		>
-		<link rel="apple-touch-icon" sizes="144x144"
-			  href="<?php echo _RESOURCESURL_; ?>img/favicons/apple-icon-144x144.png"
-		>
-		<link rel="apple-touch-icon" sizes="152x152"
-			  href="<?php echo _RESOURCESURL_; ?>img/favicons/apple-icon-152x152.png"
-		>
-		<link rel="apple-touch-icon" sizes="180x180"
-			  href="<?php echo _RESOURCESURL_; ?>img/favicons/apple-icon-180x180.png"
-		>
-		<link rel="icon" type="image/png" sizes="192x192"
-			  href="<?php echo _RESOURCESURL_; ?>img/favicons/android-icon-192x192.png"
-		>
-		<link rel="icon"
-			  type="image/png"
-			  sizes="32x32"
-			  href="<?php echo _RESOURCESURL_; ?>img/favicons/favicon-32x32.png"
-		>
-		<link rel="icon"
-			  type="image/png"
-			  sizes="96x96"
-			  href="<?php echo _RESOURCESURL_; ?>img/favicons/favicon-96x96.png"
-		>
-		<link rel="icon"
-			  type="image/png"
-			  sizes="16x16"
-			  href="<?php echo _RESOURCESURL_; ?>img/favicons/favicon-16x16.png"
-		>
-		<link rel="manifest" href="<?php echo _RESOURCESURL_; ?>img/favicons/manifest.json">
+
+        <link rel="apple-touch-icon" sizes="180x180" href="<?php echo _RESOURCESURL_; ?>img/favicons/apple-touch-icon.png">
+        <link rel="icon" type="image/png" sizes="32x32" href="<?php echo _RESOURCESURL_; ?>img/favicons/favicon-32x32.png">
+        <link rel="icon" type="image/png" sizes="16x16" href="<?php echo _RESOURCESURL_; ?>img/favicons/favicon-16x16.png">
+        <link rel="manifest" href="<?php echo _RESOURCESURL_; ?>img/favicons/site.webmanifest">
 		<meta name="msapplication-TileColor" content="#ffffff">
 		<meta name="msapplication-TileImage" content="<?php echo _RESOURCESURL_; ?>img/favicons/ms-icon-144x144.png">
 		<meta name="theme-color" content="#ffffff">
