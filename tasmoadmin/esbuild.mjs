67980225343f0e04f8212ae7a03be17c7dff7e9c
Malachi Soord <inverse.chi@gmail.com>
2024-02-03 23:00:51 +0100

    Watch foundations

--- tasmoadmin/esbuild.mjs
+++ tasmoadmin/esbuild.mjs
@@ -5,35 +5,51 @@ const scssPaths = ["resources/scss/all.scss"];
 
 const jsPaths = ["resources/js/*.js"];
 
+const watch = false;
+
 async function buildCSS() {
-  await esbuild
-    .build({
-      entryPoints: scssPaths,
-      bundle: true,
-      outfile: "resources/css/compiled/all.css",
-      loader: {
-        ".ttf": "file",
-        ".otf": "file",
-        ".svg": "file",
-        ".eot": "file",
-        ".woff": "file",
-        ".woff2": "file",
-      },
-      plugins: [sassPlugin()],
-    })
-    .then(() => console.log("⚡ CSS Build complete! ⚡"))
-    .catch(() => process.exit(1));
+  const options = {
+    entryPoints: scssPaths,
+    bundle: true,
+    outfile: "resources/css/compiled/all.css",
+    loader: {
+      ".ttf": "file",
+      ".otf": "file",
+      ".svg": "file",
+      ".eot": "file",
+      ".woff": "file",
+      ".woff2": "file",
+    },
+    plugins: [sassPlugin()],
+  };
+
+  if (watch) {
+    const ctx = await esbuild.context(options);
+    await ctx.watch();
+    console.log("⚡ CSS Watching! ⚡");
+    return;
+  }
+
+  await esbuild.build(options);
+  console.log("⚡ CSS Build complete! ⚡");
 }
 
 async function buildJS() {
-  await esbuild
-    .build({
-      entryPoints: jsPaths,
-      bundle: true,
-      outdir: "resources/js/compiled/",
-    })
-    .then(() => console.log("⚡ JS Build complete! ⚡"))
-    .catch(() => process.exit(1));
+  const options = {
+    entryPoints: jsPaths,
+    bundle: true,
+    outdir: "resources/js/compiled/",
+  };
+
+  if (watch) {
+    const ctx = await esbuild.context(options);
+    await ctx.watch();
+    console.log("⚡ JS Watching! ⚡");
+    return;
+  }
+
+  await esbuild.build(options);
+  console.log("⚡ JS Build complete! ⚡");
 }
 
 await buildCSS();
