edad8739acdecbcb777dfd1d6eda618b134f09e8
Malachi Soord <inverse.chi@gmail.com>
2025-07-22 22:06:00 +0200

    Bump alpine to 3.22 (#1398)

--- .docker/docker.sh
+++ .docker/docker.sh
@@ -3,7 +3,7 @@ set -o errexit
 
 TARGET=ghcr.io/tasmoadmin/tasmoadmin
 QEMU_VERSION=v6.1.0-8
-ALPINE_VERSION=3.21
+ALPINE_VERSION=3.22
 BUILD_REF="${BUILD_REF:=dev}"
 BUILD_VERSION="${BUILD_VERSION:=dev}"
 
